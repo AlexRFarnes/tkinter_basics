40e95377ce51c54ba2427c6d4491436c66885f3c
AlexRFarnes <alexrfarnes@gmail.com>
2025-07-12 08:31:00 +0800

    animations

--- styling/images.py
+++ styling/images.py
@@ -1,36 +1,94 @@
-# images.py
-
 import tkinter as tk
 from tkinter import ttk
 
 import customtkinter as ctk
 from PIL import Image, ImageTk
 
-# Window
+
+def stretch_image(event):
+    global resized_tk
+
+    # size
+    width = event.width
+    height = event.height
+
+    # create an image
+    resized_image = image_original.resize((width, height))
+    resized_tk = ImageTk.PhotoImage(resized_image)
+
+    # place on the canvas
+    canvas.create_image(0, 0, image=resized_tk, anchor="nw")
+
+
+def fill_image(event):
+    global resized_tk
+
+    # current ratio
+    canvas_ratio = event.width / event.height
+
+    # get coordinates
+    if canvas_ratio > image_ratio:  # canvas is wider than the image
+        width = int(event.width)
+        height = int(width / image_ratio)
+    else:  # canvas is narrower than the image
+        height = int(event.height)
+        width = int(height * image_ratio)
+
+    resized_image = image_original.resize((width, height))
+    resized_tk = ImageTk.PhotoImage(resized_image)
+    canvas.create_image(
+        int(event.width / 2), int(event.height / 2), anchor="center", image=resized_tk
+    )
+
+
+def show_full_image(event):
+    global resized_tk
+
+    # current ratio
+    canvas_ratio = event.width / event.height
+
+    # get coordinates
+    if canvas_ratio > image_ratio:  # canvas is wider than the image
+        height = int(event.height)
+        width = int(height * image_ratio)
+    else:  # canvas is narrower than the image
+        width = int(event.width)
+        height = int(width / image_ratio)
+
+    resized_image = image_original.resize((width, height))
+    resized_tk = ImageTk.PhotoImage(resized_image)
+    canvas.create_image(
+        int(event.width / 2), int(event.height / 2), anchor="center", image=resized_tk
+    )
+
+
+# exercise:
+# create a third scaling behaviour to always show the full image without cutting off parts
+
+# setup
 window = tk.Tk()
 window.geometry("600x400")
 window.title("Images")
 
-# Grid layout
+# grid layout
 window.columnconfigure((0, 1, 2, 3), weight=1, uniform="a")
 window.rowconfigure(0, weight=1)
 
-# Image
-image = Image.open("./raccoon.jpg").resize((600, 400))
-image_tk = ImageTk.PhotoImage(image)  # Required to work with Tkinter
+# import an image
+image_original = Image.open("raccoon.jpg")
+image_ratio = image_original.size[0] / image_original.size[1]
+image_tk = ImageTk.PhotoImage(image_original)
 
 python_dark = Image.open("python_dark.png").resize((30, 30))
 python_dark_tk = ImageTk.PhotoImage(python_dark)
 
 img_ctk = ctk.CTkImage(
-    light_image=Image.open("python_light.png"),
-    dark_image=Image.open("python_dark.png"),
+    light_image=Image.open("python_dark.png"), dark_image=Image.open("python_light.png")
 )
 
-# Widgets
-# label = ttk.Label(window, text="Racoon", image=image_tk)
+# widget
+# label = ttk.Label(window, text = 'raccoon', image = image_tk)
 # label.pack()
-
 button_frame = ttk.Frame(window)
 button = ttk.Button(
     button_frame, text="   A button", image=python_dark_tk, compound="left"
@@ -44,7 +102,7 @@ button_ctk.pack(pady=10)
 
 button_frame.grid(column=0, row=0, sticky="nsew")
 
-# Canvas -> image
+# canvas -> image
 canvas = tk.Canvas(
     window, background="black", bd=0, highlightthickness=0, relief="ridge"
 )
@@ -52,5 +110,5 @@ canvas.grid(column=1, columnspan=3, row=0, sticky="nsew")
 
 canvas.bind("<Configure>", show_full_image)
 
-# Run
+# run
 window.mainloop()
